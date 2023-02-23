d52e6300c0b1e14b32fdf5d98ecd3dd8b97165b1
otoyo <hiroki.t.1988@gmail.com>
2023-02-23 17:49:42 +0900

    Add constant for Lightbox

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -6,7 +6,11 @@ export const BASE_PATH = process.env.BASE_PATH || '' // <- Set sub directory pat
 
 export const PUBLIC_GA_TRACKING_ID = import.meta.env.PUBLIC_GA_TRACKING_ID
 export const NUMBER_OF_POSTS_PER_PAGE = 10
-export const REQUEST_TIMEOUT_MS = parseInt(import.meta.env.REQUEST_TIMEOUT_MS || '30000', 10)
+export const REQUEST_TIMEOUT_MS = parseInt(
+  import.meta.env.REQUEST_TIMEOUT_MS || '10000',
+  10
+)
+export const ENABLE_LIGHTBOX = import.meta.env.ENABLE_LIGHTBOX
 
 export const PUBLIC_SITE_TITLE = 'うきゅきゅ'
 export const PUBLIC_SITE_DESCRIPTION = 'astro-notion-blog is generated statically by Astro'
